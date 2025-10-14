add40ecd968c994e5088a6b6e7a3a461f30a049e
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2025-10-14 22:33:40 +0600

    Add jwt auth

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'create:user',
+    description: 'Create a new user',
+)]
+class CreateUserCommand extends Command
+{
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher,
+        private EntityManagerInterface $em,
+    )
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $data = [
+            'email' => 'user@mail.com',
+            'password' => 'password',
+        ];
+
+        $user = new User();
+        $user->setEmail($data['email']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, $data['password']));
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        return Command::SUCCESS;
+    }
+}
